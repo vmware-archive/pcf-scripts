0ec23abcf3b6b4dd3e92da729cebfbe1cc90f75e
Jim Park <spark@pivotal.io>
2017-08-29 22:14:07 +0000

    more simple

--- helper.sh
+++ helper.sh
@@ -8,22 +8,9 @@
 
 source .venv/bin/activate
 
-# netcat options aren't portable
-if [ $(nc -v 2>&1 | grep -c netcat-openbsd) -ne 0 ]; then
-  # e.g. on jumpbox
-  NC_TIMEOUT="-w 1"
-else
-  NC_TIMEOUT="-G 1"
-fi
-
-nc ${NC_TIMEOUT} -z ${BOSH_IP} 25555 && {
-  echo "Log in with:"
-} || {
-  echo Unable to ping BOSH director. start ssh tunnel by performing the following:
-  echo "scripts/<environment>/setup_tunnel"
-  echo or scripts/jumpbox/setup_tunnel
-  echo "then log in with:"
-}
+echo "scripts/<environment>/setup_tunnel"
+echo or scripts/jumpbox/setup_tunnel
+echo "then log in with:"
 
 echo scripts/opsman/login
 echo scripts/cf/login
